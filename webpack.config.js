4591f00a74270c03ada00a12f12c0e5480b78286
zqs <zhuqingsong@weihaizixun.com>
2020-07-05 19:03:21 +0800

    es6支持

--- webpack.config.js
+++ webpack.config.js
@@ -16,37 +16,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
       }
     ]
-  },
-  resolve: {
-    alias: {
-      // 'vue$': 'vue/dist/vue.esm.js'
-    },
-    extensions: ['*', '.js', '.vue', '.json']
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
   },
   performance: {
     hints: false
